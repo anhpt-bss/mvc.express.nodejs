4bb15272a0477f7c9db48ed1ffa83745acf1c9fb
anhpt <anhpt@basebs.com>
2024-07-15 09:23:32 +0700

    Update dashboard

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var asideToggle = document.getElementById('aside-toggle');
-    var navToggle = document.getElementById('nav-toggle');
-    var collapseToggle = document.getElementById('collapse-toggle');
-    var adminAside = document.getElementById('admin-aside');
-    var menuItems = document.querySelectorAll('.menu-item');
+    const asideToggle = document.getElementById('aside-toggle');
+    const navToggle = document.getElementById('nav-toggle');
+    const collapseToggle = document.getElementById('collapse-toggle');
+    const adminAside = document.getElementById('admin-aside');
+    const menuItems = document.querySelectorAll('.menu-item');
 
     asideToggle.addEventListener('click', function () {
         adminAside.classList.toggle('active');
@@ -15,11 +15,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
     collapseToggle.addEventListener('click', function () {
         adminAside.classList.toggle('collapsed');
+        localStorage.setItem('mvc_admin_aside', adminAside.classList[1] || '');
     });
 
-    var currentPath = window.location.pathname;
+    // Set menu collapsed
+    if(localStorage.getItem('mvc_admin_aside') === 'collapsed') {
+        adminAside.classList.toggle('collapsed');
+    }
+
+    // Set menu active
+    const currentPath = window.location.pathname;
     menuItems.forEach(function (item) {
-        if (item.getAttribute('data-link') === currentPath) {
+        if (
+            (item.getAttribute('data-link') === '/admin' && item.getAttribute('data-link') === currentPath) || 
+            (item.getAttribute('data-link') !== '/admin' && (item.getAttribute('data-link') === currentPath || currentPath.includes(item.getAttribute('data-link'))))
+        ) {
             item.classList.add('active');
         }
     });
